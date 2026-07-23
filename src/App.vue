36ab8ef22d0933459978a777f9e3f06a4a6803fc
BaoDHT-dev <baob2410654@student.ctu.edu.vn>
2026-07-23 23:36:48 +0700

    Tao trang cap nhat lien he

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script>
 import AppHeader from "@/components/AppHeader.vue";
 export default {
-components: {
-AppHeader,
-},
+    components: {
+        AppHeader,
+    },
 };
 </script>
 <template>
-<div id="app">
-<AppHeader />
-<div class="container mt-3">
-<router-view />
-</div>
-</div>
+    <div id="app">
+        <AppHeader />
+        <div class="container-fluid mt-3">
+            <router-view />
+        </div>
+    </div>
+
 </template>
 <style>
 .page {
-max-width: 400px;
-margin: auto;
+    max-width: 100%;
+    margin: auto;
 }
 </style>
